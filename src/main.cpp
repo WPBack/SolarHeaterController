ca2cf8f1c0bfd1fe9c95dbbee5365963b7a66014
Daniel Bäck <backd@student.chalmers.se>
2017-11-05 14:31:52 +0100

    Added send to wire to be able to datalog on a rpi

--- src/main.cpp
+++ src/main.cpp
@@ -17,58 +17,44 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Adafruit_MAX31865.h>
+#include <Wire.h>
 
 // Use software SPI: CS, DI, DO, CLK
 Adafruit_MAX31865 max = Adafruit_MAX31865(10);
 // use hardware SPI, just pass in the CS pin
 //Adafruit_MAX31865 max = Adafruit_MAX31865(10);
 
+//Variable to hold the temperature
+double lastTemp = 0;
+
+//Variable to hold the last time it was reading the temperature
+unsigned long readMillis = 0;
+
 // The value of the Rref resistor. Use 430.0!
 #define RREF 4300.0
+#define NOMREF 1000
+#define SLAVE_ADDRESS 0x04
 
+void sendData();
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
 
   max.begin(MAX31865_2WIRE);  // set to 2WIRE or 4WIRE as necessary
+  Wire.begin(SLAVE_ADDRESS);
+
+  Wire.onRequest(sendData);
 }
 
 
 void loop() {
-  uint16_t rtd = max.readRTD();
-
-  Serial.print("RTD value: "); Serial.println(rtd);
-  float ratio = rtd;
-  ratio /= 32768;
-  Serial.print("Ratio = "); Serial.println(ratio,8);
-  Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
-  Serial.print("Temperature = "); Serial.println(max.temperature(1000, RREF));
-
-  // Check and print any faults
-  uint8_t fault = max.readFault();
-  if (fault) {
-    Serial.print("Fault 0x"); Serial.println(fault, HEX);
-    if (fault & MAX31865_FAULT_HIGHTHRESH) {
-      Serial.println("RTD High Threshold");
-    }
-    if (fault & MAX31865_FAULT_LOWTHRESH) {
-      Serial.println("RTD Low Threshold");
-    }
-    if (fault & MAX31865_FAULT_REFINLOW) {
-      Serial.println("REFIN- > 0.85 x Bias");
-    }
-    if (fault & MAX31865_FAULT_REFINHIGH) {
-      Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
-    }
-    if (fault & MAX31865_FAULT_RTDINLOW) {
-      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
-    }
-    if (fault & MAX31865_FAULT_OVUV) {
-      Serial.println("Under/Over voltage");
-    }
-    max.clearFault();
+  // Read the temperature every 10 seconds
+  if (millis() - readMillis > 10000) {
+    lastTemp = max.temperature(NOMREF, RREF);
   }
-  Serial.println();
-  delay(1000);
 }
+
+void sendData() {
+    Wire.write((int)lastTemp);
+  }
