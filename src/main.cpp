65ed8aa8ef0066e4dab99b11f057245d9c8f97f3
Daniel Bäck <backd@student.chalmers.se>
2018-01-26 17:54:32 +0100

    Moved stuff out into methods

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 // Debug-mode
 #define DEBUG
 
+//-------------- Global variables ----------------------------------------------
+
 // MAX31865 sensors
 Adafruit_MAX31865 tankSensor = Adafruit_MAX31865(TANK_CS);
 Adafruit_MAX31865 panelSensor = Adafruit_MAX31865(PANEL_CS);
@@ -31,9 +33,13 @@ char tankI2C[6];
 char panelI2C[6];
 char pumpI2C[6];
 
-// Method to send the data to the raspberry for datalogging
-void sendData();
+//-------------- Help-methods --------------------------------------------------
 
+void sendData();
+void readTemps();
+void runPID();
+
+//-------------- Setup function ------------------------------------------------
 
 void setup() {
   // Serial for debug if enabled
@@ -58,48 +64,59 @@ void setup() {
   pinMode(PUMP_PIN, OUTPUT);
 }
 
+//-------------- Main loop -----------------------------------------------------
 
 void loop() {
 
   // Read the temperature
   if (millis() - readTempMillis >= READ_TEMPS_TIME) {
-
-    // Saves the time
-    readTempMillis = millis();
-
-    // Read the temps
-    tankTemp = tankSensor.temperature(NOMREF, RREF);
-    panelTemp = tankSensor.temperature(NOMREF, RREF);
-
-    // Saves the temps in char arrays to send over I2C
-    dtostrf(tankTemp, 6, 2, tankI2C);
-    dtostrf(panelTemp, 6, 2, panelI2C);
-
-    // Prints to serial if debug is enabled
-    #ifdef DEBUG
-      Serial.print("Tank: ");
-      Serial.print(tankTemp);
-      Serial.print(" Panel: ");
-      Serial.println(panelTemp);
-    #endif
+    readTemps();
   }
 
   //compute the PID
   if (millis() - pidMillis >= PID_TIME) {
-
-    // Saves the time
-    pidMillis = millis();
-
-    // Calculates the difference
-    pidInput = panelTemp - tankTemp;
-
-    // Runs the PID
-    pumpPID.Compute();
-
-    // Writes the speed to the pin that controls the pump and saves it for I2C
-    analogWrite(PUMP_PIN, pidOutput*2.55);
-    dtostrf(pidOutput, 6, 2, pumpI2C);
+    runPID();
   }
+}
+
+//-------------- Reades the tepmeratures ---------------------------------------
+
+void readTemps() {
+  // Saves the time
+  readTempMillis = millis();
+
+  // Read the temps
+  tankTemp = tankSensor.temperature(NOMREF, RREF);
+  panelTemp = tankSensor.temperature(NOMREF, RREF);
+
+  // Saves the temps in char arrays to send over I2C
+  dtostrf(tankTemp, 6, 2, tankI2C);
+  dtostrf(panelTemp, 6, 2, panelI2C);
+
+  // Prints to serial if debug is enabled
+  #ifdef DEBUG
+    Serial.print("Tank: ");
+    Serial.print(tankTemp);
+    Serial.print(" Panel: ");
+    Serial.println(panelTemp);
+  #endif
+}
+
+//-------------- Updates the PID and sets the pump speed -----------------------
+
+void runPID() {
+  // Saves the time
+  pidMillis = millis();
+
+  // Calculates the difference
+  pidInput = panelTemp - tankTemp;
+
+  // Runs the PID
+  pumpPID.Compute();
+
+  // Writes the speed to the pin that controls the pump and saves it for I2C
+  analogWrite(PUMP_PIN, pidOutput*2.55);
+  dtostrf(pidOutput, 6, 2, pumpI2C);
 }
 
 // Writes data to I2C for logging
@@ -107,4 +124,5 @@ void sendData() {
     // Write the temperatures to I2C
     Wire.write(tankI2C);
     Wire.write(panelI2C);
+    Wire.write(pumpI2C);
   }
--- src/settings.h
+++ src/settings.h
@@ -1,3 +1,15 @@
+// Tuning parameters for the PID-controller
+#define kP 5
+#define kI 1
+#define kD 0
+
+// General settins
+#define STARTTEMP 5   //Temp to start at
+#define SETPOINT  10  //Wanted temp difference
+#define STOPTEMP  2   //Temp to stop at
+#define PWM_MIN   14  //Minimum pump speed
+#define PWM_MAX   100 //Maximum pump speed
+
 // Pin-settings
 #define TANK_CS   10  //Tank chip select pin
 #define PANEL_CS  11  //Panel chip select pin
@@ -13,13 +25,3 @@
 // Settings for how often the stuff should run
 #define READ_TEMPS_TIME 5000
 #define PID_TIME 5000
-
-// Tuning parameters for the PID-controller
-#define kP 5
-#define kI 1
-#define kD 0
-
-// General settins
-#define SETPOINT 5  //Wanted temp difference
-#define PWM_MIN 14  //Minimum pump speed
-#define PWM_MAX 100 //Maximum pump speed
