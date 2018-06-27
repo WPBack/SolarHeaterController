ea585131d7596225fa3fb922c1099f924475b0aa
WPBack <daniel.a.back@telia.com>
2018-06-27 11:47:57 +0200

    Switched the direction of the PID

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ Adafruit_MAX31865 panelSensor = Adafruit_MAX31865(PANEL_CS);
 double pidSetpoint = SETPOINT;
 double pidInput = 0;
 double pidOutput = 0;
-myPID pumpPID(&pidSetpoint, &pidOutput, &pidInput, kP, kI, kD, REVERSE);
+myPID pumpPID(&pidSetpoint, &pidOutput, &pidInput, kP, kI, kD, NORMAL);
 
 // OneWire-sensors
 OneWire oneWire(ONE_WIRE_PIN);
